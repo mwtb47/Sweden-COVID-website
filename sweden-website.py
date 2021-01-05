57f0e2c9e20c6a417fdd9607c9ded89342523954
mwtb47 <mattb47@live.co.uk>
2021-01-05 14:21:09 +0100

    Update sweden-website.py
    - Spelling correction

--- sweden-website.py
+++ sweden-website.py
@@ -20,7 +20,7 @@ import urllib
 # =============================================================================
 
 # -----------------------------------
-# Read data form Folkhälsomyndigheten
+# Read data from Folkhälsomyndigheten
 # -----------------------------------
 
 folkhälsomyndigheten_url = "https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data"
